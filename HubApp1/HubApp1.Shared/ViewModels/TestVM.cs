d2e1f1985e9139234df27ad3d6d1ba1992d43213
malliina <malliina123@gmail.com>
2015-01-25 21:00:17 +0200

    device name for universal apps

--- HubApp1/HubApp1.Shared/ViewModels/TestVM.cs
+++ HubApp1/HubApp1.Shared/ViewModels/TestVM.cs
@@ -1,10 +1,24 @@
 ﻿
+using Windows.Security.ExchangeActiveSyncProvisioning;
 namespace HubApp1.ViewModels {
     public class TestVM : ViewModelBase {
         private string text = "Hello, world!";
         public string Text {
             get { return text; }
             set { SetProperty(ref text, value); }
+        }
+        private string deviceName;
+        public string DeviceName {
+            get { return deviceName; }
+        }
+        private string os;
+        public string OS {
+            get { return os; }
+        }
+        public TestVM() {
+            var deviceInformation = new EasClientDeviceInformation();
+            deviceName = deviceInformation.FriendlyName;
+            os = deviceInformation.OperatingSystem;
         }
     }
 }
