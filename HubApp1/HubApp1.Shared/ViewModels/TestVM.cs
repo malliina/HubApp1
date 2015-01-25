81792d3ce4966e043aca818e8df8780641ed203a
malliina <malliina123@gmail.com>
2015-01-25 20:38:32 +0200

    init

---
+++ HubApp1/HubApp1.Shared/ViewModels/TestVM.cs
@@ -0,0 +1,10 @@
+﻿
+namespace HubApp1.ViewModels {
+    public class TestVM : ViewModelBase {
+        private string text = "Hello, world!";
+        public string Text {
+            get { return text; }
+            set { SetProperty(ref text, value); }
+        }
+    }
+}
